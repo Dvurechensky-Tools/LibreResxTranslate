1ea15a67129f9d18f0c3662c4bd781950b15d05b
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-05-07 15:47:46 +0300

    Auto-update headers via pre_commit.ps1 - 2026-05-07

--- app/LibreResxTranslate.Tests/MSTestSettings.cs
+++ app/LibreResxTranslate.Tests/MSTestSettings.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 06 мая 2026 10:51:51
- * Version: 1.0.18
+ * Last Updated: 07 мая 2026 15:47:43
+ * Version: 1.0.19
  */
 
 [assembly: Parallelize(Scope = ExecutionScope.MethodLevel)]
