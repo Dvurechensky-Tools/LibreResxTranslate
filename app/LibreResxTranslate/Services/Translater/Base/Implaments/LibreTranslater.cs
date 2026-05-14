168812675c5770a84a1397cc8f7974fe32f1a570
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-05-14 10:56:48 +0300

    Auto-update headers via pre_commit.ps1 - 2026-05-14

--- app/LibreResxTranslate/Services/Translater/Base/Implaments/LibreTranslater.cs
+++ app/LibreResxTranslate/Services/Translater/Base/Implaments/LibreTranslater.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 13 мая 2026 13:36:43
- * Version: 1.0.25
+ * Last Updated: 14 мая 2026 10:56:45
+ * Version: 1.0.26
  */
 
 using System.Text;
