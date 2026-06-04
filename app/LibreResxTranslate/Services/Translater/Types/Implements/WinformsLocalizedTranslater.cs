a016aca86f42b3346cac6534b55c5db7bbbaef78
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-04 07:13:56 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-04

--- app/LibreResxTranslate/Services/Translater/Types/Implements/WinformsLocalizedTranslater.cs
+++ app/LibreResxTranslate/Services/Translater/Types/Implements/WinformsLocalizedTranslater.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 03 июня 2026 15:38:37
- * Version: 1.0.45
+ * Last Updated: 04 июня 2026 07:13:53
+ * Version: 1.0.46
  */
 
 using System.Collections;
