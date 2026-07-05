cc70a4342555c636fbcc0c24ac631b7fa210724d
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-05 11:15:10 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-05

--- app/LibreResxTranslate/Services/Base/Extensions.cs
+++ app/LibreResxTranslate/Services/Base/Extensions.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 04 июля 2026 09:00:29
- * Version: 1.0.75
+ * Last Updated: 05 июля 2026 11:15:08
+ * Version: 1.0.76
  */
 
 public class Extensions
