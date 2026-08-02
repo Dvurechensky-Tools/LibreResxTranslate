a9f88b9e631112938b087a37bf417a9a3f7ebdf2
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-02 07:15:15 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-02

--- app/LibreResxTranslate/Services/Settings/implements/SettingsService.cs
+++ app/LibreResxTranslate/Services/Settings/implements/SettingsService.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 01 августа 2026 07:18:38
- * Version: 1.0.103
+ * Last Updated: 02 августа 2026 07:15:12
+ * Version: 1.0.104
  */
 
 using Newtonsoft.Json;
