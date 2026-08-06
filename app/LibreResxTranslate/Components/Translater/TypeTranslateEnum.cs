40de5a73ef29fe73a7315d8de7a6b4916db9d17e
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-06 07:21:31 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-06

--- app/LibreResxTranslate/Components/Translater/TypeTranslateEnum.cs
+++ app/LibreResxTranslate/Components/Translater/TypeTranslateEnum.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 05 августа 2026 07:14:56
- * Version: 1.0.107
+ * Last Updated: 06 августа 2026 07:21:27
+ * Version: 1.0.108
  */
 
 namespace LibreResxTranslate.Components;
