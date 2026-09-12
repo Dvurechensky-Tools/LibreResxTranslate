d9b233f6955fb3981fc399db3e098a5e7703da93
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-09-12 06:58:51 +0300

    Auto-update headers via pre_commit.ps1 - 2026-09-12

--- app/LibreResxTranslate.Tests/SettingsServiceTests.cs
+++ app/LibreResxTranslate.Tests/SettingsServiceTests.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 11 сентября 2026 06:58:42
- * Version: 1.0.143
+ * Last Updated: 12 сентября 2026 06:58:48
+ * Version: 1.0.144
  */
 
 using LibreResxTranslate.Services.Settings;
